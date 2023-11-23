997c4a2220c2eb3e143333093dc5e24687be3ce3
Marianna <Marianna.lopes@academico.ifpb.edu.br>
2023-11-23 18:55:00 -0300

    Definindo esquema Prisma

---
+++ src/@types/express.d.ts
@@ -0,0 +1,31 @@
+type Imovel = {
+    id: string,
+  nome: string
+  latitude: number,
+  longitude: number,
+  tipo: string
+  descricao: string,
+  preco: number,
+  disponivel: boolean
+  avaliacao: number
+  numInquilinos: number
+}
+
+type Usuario = {
+    id: string,
+  nome: string,
+  username: string,
+  senha: string,
+  telefone: string,
+  email: string
+}
+
+type Imagens = {
+    nomeImagem: string
+}
+
+declare namespace Express{
+    export interface Request{
+        user: Usuario;
+    }
+}
