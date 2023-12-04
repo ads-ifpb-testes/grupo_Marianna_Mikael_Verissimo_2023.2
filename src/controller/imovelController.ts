f8d9521fce2febbff80a0300d322f2d78082e0f1
mikael <mikael.stl05@gmail.com>
2023-12-04 15:30:55 -0300

    controller do imovel finalizado

--- src/controller/imovelController.ts
+++ src/controller/imovelController.ts
@@ -14,6 +14,56 @@ export class ImovelController {
     return res.status(200).json(resp);
   }
 
+  static async findByType(req: Request, res: Response){
+    const { tipo } = req.params as { tipo: string };
+
+    const resp = await ImovelHandle.findByType(tipo);
+
+    res.status(200).json(resp);
+  }
+
+  static async findByName(req: Request, res: Response){
+    const { nome } = req.params as { nome: string };
+
+    const resp = await ImovelHandle.findByName(nome);
+
+    res.status(200).json(resp);
+  }
+
+  static async findByLocale(req: Request, res: Response){
+    const coords = req.body as { lng: number, lat: number };
+
+    const resp = await ImovelHandle.findByLocale(coords);
+
+    res.status(200).json(resp);
+  }
+
+  static async updateName(req: Request, res: Response){
+    const { nome } = req.body;
+    const { id } = req.params;
+
+    const resp = await ImovelHandle.updateName(nome, id);
+
+    res.status(resp.status).json(resp.message);
+  }
+
+  static async updateLocale(req: Request, res: Response){
+    const { id } = req.params;
+
+    const resp = await ImovelHandle.updateLocale(req.body, id);
+
+    res.status(resp.status).json(resp.message);
+  }
+
+  static async updateAvailability(req: Request, res: Response){
+    const { disponivel } = req.body as { disponivel: boolean};
+    const { id } = req.params;
+
+    const resp = await ImovelHandle.updateAvailability(disponivel, id);
+
+    res.status(resp.status).json(resp.message);
+  }
+
   static async delete(req: Request, res: Response){
     const { id } = req.params;
 
